f7b22c7a5de0d38990af009a6868f12e78a790a3
spencer0124 <spencer0124@naver.com>
2024-01-09 12:22:10 +0900

    chore: env setting

--- src/App.js
+++ src/App.js
@@ -14,23 +14,6 @@ function App() {
     setScreenSize();
   });
 
-  // <div className="App">
-  //   <header className="App-header">
-  //     <Hello />
-  //     <img src={logo} className="App-logo" alt="logo" />
-  //     <p>
-  //       Edit <code>src/App.js</code> and save to reload.
-  //     </p>
-  //     <a
-  //       className="App-link"
-  //       href="https://reactjs.org"
-  //       target="_blank"
-  //       rel="noopener noreferrer"
-  //     >
-  //       Learn React
-  //     </a>
-  //   </header>
-  // </div>
   return (
     <Routes>
       <Route path="/" element={<ErrorPage />} />
